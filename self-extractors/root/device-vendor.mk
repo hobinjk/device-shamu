7db3d3eac9a1be239523b4f4df1a393f305dda6a
Pierre-Eric Pelloux-Prayer <pierre-eric@phoxygen.com>
2015-05-12 14:42:54 +0200

    Exclude BuaContactAdapter.apk from build
    It depends on out/host/linux-x86/framework/signapk.jar which is not built

--- self-extractors/root/device-vendor.mk
+++ self-extractors/root/device-vendor.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_PACKAGES += \
-    BuaContactAdapter
+#PRODUCT_PACKAGES += \
+#    BuaContactAdapter
 
 LOCAL_STEM := shamu/device-partial.mk
 
